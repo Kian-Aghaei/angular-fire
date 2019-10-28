f03b436c3b461a82a18c06708c950e9ab29ade55
Kian-Aghaei <37697445+Kian-Aghaei@users.noreply.github.com>
2019-10-28 10:26:19 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,12 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyCJCOWRseAgnX_1g5DPio9HENQYAwcK29c',
-    authDomain: 'angular-fire-b021c.firebaseapp.com',
-    databaseURL: 'https://angular-fire-b021c.firebaseio.com',
-    projectId: 'angular-fire-b021c',
-    storageBucket: 'angular-fire-b021c.appspot.com',
-    messagingSenderId: '388329363049'
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: ''
   },
 };
 
